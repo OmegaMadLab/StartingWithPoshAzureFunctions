a66fbd8e16fa24d526c7d04b4ce16ea97d46d5a2
Marco Obinu <locurtola@tin.it>
2019-10-25 23:00:01 +0200

    Update run.ps1

--- changeVmState/run.ps1
+++ changeVmState/run.ps1
@@ -42,8 +42,9 @@ switch ($action) {
     }
 }
 
-# # Associate values to output bindings by calling 'Push-OutputBinding'.
-# Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-#     StatusCode = $status
-#     Body = $body
-# })
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body = $body
+})
+
